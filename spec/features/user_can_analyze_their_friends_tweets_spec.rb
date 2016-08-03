d5adb114b9ad7c60dc05f3b6b9a6162d43596ccc
Deborah Hamel <deborahleehamel@gmail.com>
2016-08-02 19:22:47 -0600

    add Back to Dashboard button to nav create shared folder in views for partials add partial for nav add partial for tweet_anlaysis_table remove score from table display update homepage text

---
+++ spec/features/user_can_analyze_their_friends_tweets_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "user can analyze their friends tweets" do
+  pending
+  # scenario "returns page with 20 random friends" do
+  #
+  #   click_on "Analyze Tweets"
+  #
+  #   expect(current_path).to eq "/tweets"
+  #
+  #   expect(page).to have_content("Select a friend and analyze their tweets")
+  #   expect(page).to have_content("Back to Dashboard")
+  #   expect(page).to have_content("Positive")
+  #   expect(page).to have_content("Negative")
+  #   expect(page).to have_content("RT")
+  #   expect(page).to have_content("@")
+  # end
+end
